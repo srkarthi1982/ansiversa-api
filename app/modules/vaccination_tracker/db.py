4e81dae76ea887e494ed044189382f814ba7cfb9
srkarthi1982 <srkarthi1982@gmail.com>
2026-07-15 18:07:26 +0400

    Fix vaccination database auth

--- app/modules/vaccination_tracker/db.py
+++ app/modules/vaccination_tracker/db.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.core.config import settings
-from app.core.database import _build_database_url, _build_engine_kwargs
+from app.core.database import _build_connect_args, _build_database_url, _build_engine_kwargs
 
 class VaccinationBase(DeclarativeBase):
     pass
 
 vaccination_engine = create_engine(
     _build_database_url(settings.VACCINATION_TRACKER_DATABASE_URL),
+    connect_args=_build_connect_args(settings.VACCINATION_TRACKER_DATABASE_URL),
     pool_pre_ping=True,
     **_build_engine_kwargs(settings.VACCINATION_TRACKER_DATABASE_URL),
 )
